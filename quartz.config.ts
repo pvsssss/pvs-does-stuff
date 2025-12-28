9c8ab484ae28278f29d0879f19cb92f244a186e4
Pratakhveer Singh <pratakhveersingh@gmail.com>
2025-12-28 20:08:32 +0530

    Quartz sync: Dec 28, 2025, 8:08 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "pvsssss.github.io/pvs-does-stuff/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
