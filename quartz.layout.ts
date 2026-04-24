fd38d033c3e584ec1e5f46513b6738e27a2ab6c8
Pratakhveer Singh <pratakhveersingh@gmail.com>
2026-04-24 16:36:10 +0530

    Quartz sync: Apr 24, 2026, 4:36 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,12 +1,13 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 import ScrollProgress from "./quartz/components/ScrollProgress"
+import Oneko from "./quartz/components/Oneko"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [ScrollProgress()],  // here
+  afterBody: [ScrollProgress(),Oneko()],  // here
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/pvssss",
---
+++ quartz/components/Oneko.tsx
@@ -0,0 +1,7 @@
+import { QuartzComponentConstructor } from "./types"
+
+export default (() => {
+  return () => (
+    <script src="/static/oneko.js" async />
+  )
+}) satisfies QuartzComponentConstructor
