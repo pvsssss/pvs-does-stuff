9e771f9ae34805465c61183d24a625d688be1931
Pratakhveer Singh <pratakhveersingh@gmail.com>
2026-04-24 17:01:39 +0530

    Quartz sync: Apr 24, 2026, 5:01 PM

--- quartz/components/Oneko.tsx
+++ quartz/components/Oneko.tsx
@@ -2,6 +2,17 @@ import { QuartzComponentConstructor } from "./types"
 
 export default (() => {
   return () => (
-    <script src="/pvs-does-stuff/static/oneko.js" async />
+    <>
+      <script src="/pvs-does-stuff/static/oneko.js" async />
+      <script dangerouslySetInnerHTML={{ __html: `
+        document.addEventListener("nav", () => {
+          const existing = document.getElementById("oneko");
+          if (existing) existing.remove();
+          const script = document.createElement("script");
+          script.src = "/pvs-does-stuff/static/oneko.js";
+          document.body.appendChild(script);
+        });
+      `}} />
+    </>
   )
 }) satisfies QuartzComponentConstructor
